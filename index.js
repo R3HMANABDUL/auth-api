683bcedc23582b8389af9e4cefd8b7300e3f1dad
R3HMANABDUL <abdulr3hman0000@gmail.com>
2025-08-31 17:07:19 +0500

    this commite

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-const express = require('express');
+require("dotenv").config();  // load .env first!
+
+const express = require("express");
+const mongoose = require("mongoose");
+
 const app = express();
-const port = process.env.PORT || 3000;
+
+// Read from .env only
+const PORT = process.env.PORT;
+const MONGO_URI = process.env.MONGO_URI;
 
 
+mongoose.connect(MONGO_URI)
+  .then(() => console.log("✅ MongoDB connected"))
+  .catch(err => console.error("❌ MongoDB error:", err));
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`✅ Server running `);
 });
